b7c0dd7f9ec1fe902632fff9ca3eff38fc97c25e
afscrome <afscrome@users.noreply.github.com>
2026-09-15 22:36:05 +0100

    Add management resource relationships

--- src/Aspire.Hosting.Kafka/KafkaBuilderExtensions.cs
+++ src/Aspire.Hosting.Kafka/KafkaBuilderExtensions.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Aspire.Hosting.ApplicationModel;
+using Aspire.Dashboard.Model;
 using Confluent.Kafka;
 using HealthChecks.Kafka;
 using Microsoft.Extensions.DependencyInjection;
@@ -101,7 +102,7 @@ public static class KafkaBuilderExtensions
         {
             var builderForExistingResource = builder.ApplicationBuilder.CreateResourceBuilder(existingKafkaUIResource);
             configureContainer?.Invoke(builderForExistingResource);
-            builderForExistingResource.WithRelationship(builder.Resource, "KafkaUI");
+            builderForExistingResource.WithRelationship(builder.Resource, KnownRelationshipTypes.Manages);
             return builder;
         }
         else
@@ -137,7 +138,7 @@ public static class KafkaBuilderExtensions
 
             configureContainer?.Invoke(kafkaUiBuilder);
 
-            kafkaUiBuilder.WithRelationship(builder.Resource, "KafkaUI");
+            kafkaUiBuilder.WithRelationship(builder.Resource, KnownRelationshipTypes.Manages);
 
             return builder;
         }
@@ -170,6 +171,11 @@ public static class KafkaBuilderExtensions
         {
             foreach (var kafkaResource in @event.Model.Resources.OfType<KafkaServerResource>())
             {
+                if (!resourceBuilder.Resource.Annotations.OfType<ResourceRelationshipAnnotation>().Any(r => r.Type == KnownRelationshipTypes.Manages && r.Resource == kafkaResource))
+                {
+                    resourceBuilder.WithRelationship(kafkaResource, KnownRelationshipTypes.Manages);
+                }
+
 #pragma warning disable CS0618 // DisplayOrder is obsolete but must still be set to prioritize this URL.
                 kafkaResource.Annotations.Add(new ResourceUrlAnnotation
                 {
